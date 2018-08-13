db42dff4d4159e57917a842dc47178ff55329599
Thomas Lawton <thomaslawton91@gmail.com>
2018-08-12 23:36:18 -0400

    adding blog functionality

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.4.3'
 
+gem "builder", "~> 3.0"
 gem 'middleman', '~> 4.2'
 gem 'middleman-google-analytics', '~> 3.0'
 gem 'middleman-sprockets', '~> 4.0.0.rc'
@@ -11,9 +12,11 @@ gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-livereload', '~> 3.4'
 gem 'middleman-minify-html', '~> 3.4'
 gem 'middleman-dotenv', '~> 2.0'
+gem "middleman-blog"
 gem 'oj'
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
 gem 'slim', '~> 3.0'
 gem 'sprockets-es6'
 gem 'therubyracer'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :jruby]
--- config.rb
+++ config.rb
@@ -38,3 +38,30 @@ configure :production do
   activate :minify_html
   activate :minify_javascript
 end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+    blog.layout = "blog"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
