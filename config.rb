aa5e54647d113c4800e653a0a505b85b22eef7ab
Thomas Lawton <thomaslawton91@gmail.com>
2018-09-14 15:06:08 -0400

    finish styling of .md and complete website

--- config.rb
+++ config.rb
@@ -49,7 +49,7 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-    blog.default_extension = ".md"
+  # blog.default_extension = ".md"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
